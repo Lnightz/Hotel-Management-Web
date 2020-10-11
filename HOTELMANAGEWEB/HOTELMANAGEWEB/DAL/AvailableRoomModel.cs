28dc78a049d9fdd10b24dcaf9f7483df5d17628a
Hieu <vuonghieu5598@gmail.com>
2020-10-12 01:39:48 +0700

    day 1.1

---
+++ HOTELMANAGEWEB/HOTELMANAGEWEB/DAL/AvailableRoomModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HOTELMANAGEWEB.DAL
+{
+    public class AvailableRoomModel
+    {
+        public string RoomTypeName { get; set; }
+
+        public string RoomTypeDescription { get; set; }
+
+        public decimal RoomTypePrices { get; set; }
+    }
+}
